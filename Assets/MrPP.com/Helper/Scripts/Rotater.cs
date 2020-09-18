c3e24d428dcb0bf606385782d81c3063f64a20f0
dirui <dirui1@yeah.net>
2020-09-18 23:19:21 +0800

    no message

---
+++ Assets/MrPP.com/Helper/Scripts/Rotater.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace MrPP.Helper { 
+    public class Rotater : MonoBehaviour {
+        [SerializeField]
+        private Vector3 _eulerAngles;
+        [SerializeField]
+        private float _speed = 1f;
+      
+	
+	    // Update is called once per frame
+	    void Update () {
+            this.transform.Rotate(_eulerAngles * _speed * Time.deltaTime);
+	    }
+    }
+}
